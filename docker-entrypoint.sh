dc621e3211ee92c1d9f4163149e05c7ece9f68be
Stefan Wold <ratler@undernet.org>
2022-12-11 13:47:43 +0100

    Disable reserved blocks for local development

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -25,6 +25,7 @@ if [ "$1" == "httpd" ]; then
     sed -i '/STD_THEME/s;default;unetnew;' /app/php_includes/config.inc
     sed -i '/FONT_PATH/s;\.\.\.;/app/fonts;' /app/php_includes/config.inc
     sed -i '/LIVE_LOCATION/s;/live;;' /app/php_includes/config.inc
+    sed -i '/DISALLOW_RESERVED_BLOCKS/s;1;0;' /app/php_includes/config.inc
   fi
   if [ ! -f "/app/php_includes/blackhole.inc" ]; then
     cp "/app/php_includes/blackhole.inc.dist" "/app/php_includes/blackhole.inc"
